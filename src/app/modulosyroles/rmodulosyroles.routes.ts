1ce345f7ebf99cfe3cde789ce9c015e2e043f7e2
Luis De La Rosa <luisdelarosa0709@gmail.com>
2024-11-20 20:15:41 -0500

    Feat: agregar tablas para gestionar tipos, productos y categorías de servicios en el componente de contenido

--- src/app/modulosyroles/rmodulosyroles.routes.ts
+++ src/app/modulosyroles/rmodulosyroles.routes.ts
@@ -4,14 +4,14 @@ import { PageMainComponent } from '../pages/page-main/page-main.component';
 import { PageModulosyrolesComponent } from './pages/page-modulosyroles/page-modulosyroles.component';
 
 export const ROUTES_MODULOSYROLES: Routes = [
-  {
-    path: '',
-    component: PageMainComponent,
-    children: [
-      {
-        path: 'modulosyroles',
-        component: PageModulosyrolesComponent
-      }
-    ]
-  }
+  // {
+  //   path: '',
+  //   component: PageMainComponent,
+  //   children: [
+  //     {
+  //       path: 'modulosyroles',
+  //       component: PageModulosyrolesComponent
+  //     }
+  //   ]
+  // }
 ];
--- src/app/productosyservicios/components/content-productosyservicios/content-productosyservicios.component.ts
+++ src/app/productosyservicios/components/content-productosyservicios/content-productosyservicios.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TableTiposProdServComponent } from "../tables-productosyservicios/table-tipos-prod-serv/table-tipos-prod-serv.component";
+import { TableProductosServiciosComponent } from "../tables-productosyservicios/table-productos-servicios/table-productos-servicios.component";
+import { TableCategoriasProdServComponent } from "../tables-productosyservicios/table-categorias-prod-serv/table-categorias-prod-serv.component";
 
 @Component({
   selector: 'app-content-productosyservicios',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, TableTiposProdServComponent, TableProductosServiciosComponent, TableCategoriasProdServComponent],
   templateUrl: './content-productosyservicios.component.html',
   styleUrl: './content-productosyservicios.component.scss'
 })
 export class ContentProductosyserviciosComponent {
 
 }
+
